65de0e3cbc29e8081f6f8d917e6b48b7a1d7b318
greyepoxy <greyepoxy2@gmail.com>
2020-11-07 11:26:38 -0800

    first couple of tests

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # basic-typescript-template
 Basic TS build and test environment for Node
+
+
+## Conways game of life
+Today we'll be working on Conway's game of life.
+
+The game is a cellular automaton, where cells live or die according to a set of four rules:
+
+
+
+Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
+Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
+Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
+Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
+
+
+Here is a working example to play around with
--- test/fake.tests.ts
+++ test/fake.tests.ts
@@ -1,6 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import test from 'ava';
-import { test as testCode } from '../src';
+// Births: Each dead cell adjacent to exactly three live neighbors will become live in the next generation.
+// Dead cell with 3 neighbors will become alive
 
-test('A test test', (t) => {
-  t.deepEqual(testCode, 5);
+// Death by isolation: Each live cell with one or fewer live neighbors will die in the next generation.
+// Alive cell with 0 neighbors will die
+
+// Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
+// Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
+
+function createGrid() {
+  let cellState = true;
+  const grid = {
+    setAlive: (_x: number, _y: number) => {
+      //
+    },
+    advance: () => {
+      cellState = false;
+    },
+    isCellAlive: (_x: number, _y: number): boolean => {
+      return cellState;
+    },
+  };
+
+  return grid;
+}
+
+test('Cell can be set to alive', (t) => {
+  const grid = createGrid();
+
+  grid.setAlive(0, 0);
+
+  t.deepEqual(grid.isCellAlive(0, 0), true);
 });
+
+test('Alive cell with 0 neighbors will die', (t) => {
+  const grid = createGrid();
+
+  grid.setAlive(0, 0);
+  grid.advance();
+
+  t.deepEqual(grid.isCellAlive(0, 0), false);
+});
+
+// Death by overcrowding: Each live cell with four or more live neighbors will die in the next generation.
+// Survival: Each live cell with either two or three live neighbors will remain alive for the next generation.
